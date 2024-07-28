750eea972d0faf9e76c7047ea1f5516bd3504dea
Darsh <thakurdarsh21@gmail.com>
2024-07-28 15:55:28 +0530

    completed the sigin in screen

---
+++ src/components/CustomButton/CustomButton.js
@@ -0,0 +1,23 @@
+import { View, Text,StyleSheet } from 'react-native'
+import React from 'react'
+
+const CustomButton = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text} >Button</Text>
+    </View>
+  )
+}
+
+const styles=StyleSheet.create(
+    {
+        container:{
+
+        },
+        text:{
+
+        },
+    }
+)
+
+export default CustomButton
